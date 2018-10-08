5d764ef227249872bda975d9bb7fd650b2b29952
waynee95 <larsericmarquardt@mail.de>
2018-10-08 18:26:23 +0200

    Remove Ctrl from pinky and make it a thumb key instead
    After a while its really hard on the pinky to press Ctrl. So far, using it on
    the thumb is much better

--- keymap.c
+++ keymap.c
@@ -14,15 +14,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // layer 0 : default
     [BASE] = LAYOUT_ergodox_pretty(
-       KC_ESC,    KC_1,        KC_2, KC_3,    KC_4, KC_5, ___,        ___,  KC_6, KC_7,    KC_8,    KC_9,   KC_0,           DE_SS,
-       KC_TAB,    KC_Q,        KC_W, KC_E,    KC_R, KC_T, ___,        ___,  DE_Z, KC_U,    KC_I,    KC_O,   KC_P,           DE_UE,
-       KC_CAPS,   KC_A,        KC_S, KC_D,    KC_F, KC_G,                   KC_H, KC_J,    KC_K,    KC_L,   DE_OE,          DE_AE,
-       KC_LSHIFT, CTL_T(DE_Y), KC_X, KC_C,    KC_V, KC_B, ___,        ___,  KC_N, KC_M,    KC_COMM, KC_DOT, CTL_T(DE_MINS), KC_RSHIFT,
-       ___,       ___,          ___, KC_LEFT, KC_RIGHT,                           KC_DOWN, KC_UP,   ___,    ___,      ___,
+       KC_ESC,    KC_1, KC_2, KC_3,    KC_4, KC_5, ___,        ___,  KC_6, KC_7,    KC_8,    KC_9,   KC_0,    DE_SS,
+       KC_TAB,    KC_Q, KC_W, KC_E,    KC_R, KC_T, ___,        ___,  DE_Z, KC_U,    KC_I,    KC_O,   KC_P,    DE_UE,
+       KC_CAPS,   KC_A, KC_S, KC_D,    KC_F, KC_G,                   KC_H, KC_J,    KC_K,    KC_L,   DE_OE,   DE_AE,
+       KC_LSHIFT, DE_Y, KC_X, KC_C,    KC_V, KC_B, ___,        ___,  KC_N, KC_M,    KC_COMM, KC_DOT, DE_MINS, KC_RSHIFT,
+       ___,       ___,   ___, KC_LEFT, KC_RIGHT,                           KC_DOWN, KC_UP,   ___,    ___,      ___,
 
                                               ___, ___,        ___, ___,
-                                               DE_ALGR,        KC_LCTRL,
-                              KC_SPC, KC_BSPC, KC_LALT,        KC_DEL, MO(SYMB), KC_ENT
+                                               DE_ALGR,        KC_DEL,
+                              KC_SPC, KC_BSPC, KC_LCTRL,       KC_LALT, MO(SYMB), KC_ENT
 
     ),
 
